67cdb4c3d3f7678a07569935a6b99904f07f1af2
Alec Mather <aymather@gmail.com>
2023-03-29 00:42:21 -0700

    Added the weekly global pipeline to the scheduler

--- lib/RCAResearchTeamScheduler.py
+++ lib/RCAResearchTeamScheduler.py
@@ -3,6 +3,7 @@ from .NielsenDailyUSPipeline import NielsenDailyUSPipeline
 from .NielsenDailyGlobalPipeline import NielsenDailyGlobalPipeline
 from .NielsenWeeklyMappingTablePipeline import NielsenWeeklyMappingTablePipeline
 from .WeeklyFunctionsPipeline import WeeklyFunctionsPipeline
+from .NielsenWeeklyGlobalPipeline import NielsenWeeklyGlobalPipeline
 
 class RCAResearchTeamScheduler(SchedulerBase):
 
@@ -13,32 +14,39 @@ class RCAResearchTeamScheduler(SchedulerBase):
 
         # Sunday
         self.set('Sunday', NielsenDailyGlobalPipeline)
+        self.set('Sunday', NielsenWeeklyGlobalPipeline)
 
         # Monday
         self.set('Monday', NielsenDailyUSPipeline)
         self.set('Monday', NielsenDailyGlobalPipeline)
         self.set('Monday', NielsenWeeklyMappingTablePipeline)
+        self.set('Monday', NielsenWeeklyGlobalPipeline)
         self.set('Monday', WeeklyFunctionsPipeline)
 
         # Tuesday
         self.set('Tuesday', NielsenDailyUSPipeline)
         self.set('Tuesday', NielsenDailyGlobalPipeline)
+        self.set('Tuesday', NielsenWeeklyGlobalPipeline)
 
         # Wednesday
         self.set('Wednesday', NielsenDailyUSPipeline)
         self.set('Wednesday', NielsenDailyGlobalPipeline)
+        self.set('Wednesday', NielsenWeeklyGlobalPipeline)
 
         # Thursday
         self.set('Thursday', NielsenDailyUSPipeline)
         self.set('Thursday', NielsenDailyGlobalPipeline)
+        self.set('Thursday', NielsenWeeklyGlobalPipeline)
 
         # Friday
         self.set('Friday', NielsenDailyUSPipeline)
         self.set('Friday', NielsenDailyGlobalPipeline)
+        self.set('Friday', NielsenWeeklyGlobalPipeline)
 
         # Saturday
         self.set('Saturday', NielsenDailyUSPipeline)
         self.set('Saturday', NielsenDailyGlobalPipeline)
+        self.set('Saturday', NielsenWeeklyGlobalPipeline)
 
     def test_build(self):
 
