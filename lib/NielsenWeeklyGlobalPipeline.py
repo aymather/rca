3fd988745fdeaaefe6f5ee6f0531231aaa8ef668
Alec Mather <aymather@gmail.com>
2023-11-13 10:55:45 -0800

    ...nielsen changed their encoding back to UTF-16

--- lib/NielsenWeeklyGlobalPipeline.py
+++ lib/NielsenWeeklyGlobalPipeline.py
@@ -456,7 +456,7 @@ class NielsenWeeklyGlobalPipeline(PipelineBase):
             self.sftp_conn.get(fullfile, fullfile)
 
         # Read, clean and update
-        df = pd.read_csv(fullfile, delimiter='\t', encoding='UTF-8')
+        df = pd.read_csv(fullfile, delimiter='\t', encoding='UTF-16')
 
         return df
     
